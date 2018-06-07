749b6bf38351600c90dbc72f065d41d50dc0f6fb
lrussell21 <lrussell21@my.whitworth.edu>
2018-06-06 21:35:09 -0700

    Fixed Overlap Function.

--- HW4/EX04_04/EX04_04/EX04_04.cpp
+++ HW4/EX04_04/EX04_04/EX04_04.cpp
@@ -30,8 +30,8 @@ public:
   const double getwidth() { return width; }
   const double getheight() { return height; }
 
-  const double getArea() { return x * y; }
-  const double getPerimeter() { return (2 * x) + (2 * y); }
+  const double getArea() { return width * height; }
+  const double getPerimeter() { return (2 * width) + (2 * height); }
 
   const bool contains(double x2, double y2) {
     //Probably a more elegant way to do this but I need to finish other problems.
@@ -51,6 +51,7 @@ public:
     //top bottom left right
     Rectangle2D* ptr = new Rectangle2D();
     *ptr = r;
+    //This hurts me on the inside, but it's the only way I can think of right now.
     if ((ptr->gety() + (0.5 * ptr->getheight())) < (y + (0.5 * height))) {
       if ((ptr->gety() - (0.5 * ptr->getheight())) > (y - (0.5 * height))) {
         if ((ptr->getx() - (0.5 * ptr->getwidth())) > (x - (0.5 * width))) {
@@ -67,23 +68,34 @@ public:
     int overlapTest = 0;
     Rectangle2D* ptr = new Rectangle2D();
     *ptr = r;
-    if ((ptr->getx() - (0.5 * ptr->getwidth())) < (x + (0.5 * width)) < (ptr->getx() + (0.5 * ptr->getwidth()))) {
-      cout << "test";
+    //I got stuck on this for 45 minutes and realized I was still calling contains instead of overlaps...
+    //Coding standards in cs171 said to use 75 characters max but most screens we use now
+    //can output over 150 characters and I think splitting these if statements into 75 characters would
+    //make it very hard to read.
+
+    //These if statements test if a side of a rectangle is between the two sides on another rectangle.
+    //If they are overlapping 2 lines have to cross which is why I set overlapTest == 2 in end to output true.
+    if ((ptr->getx() - (0.5 * ptr->getwidth())) < (x + (0.5 * width)) &&
+                                                  (x + (0.5 * width)) < (ptr->getx() + (0.5 * ptr->getwidth()))) {
       overlapTest++;
     }
-    if (((ptr->getx() - (0.5 * ptr->getwidth())) < (x - (0.5 * width))) && ((x - (0.5 * width)) < (ptr->getx() + (0.5 * ptr->getwidth())))) {
+    if ((ptr->getx() - (0.5 * ptr->getwidth())) < (x - (0.5 * width)) &&
+                                                  (x - (0.5 * width)) < (ptr->getx() + (0.5 * ptr->getwidth()))) {
       overlapTest++;
     }
 
-    if ((ptr->gety() - (0.5 * ptr->getheight())) < (y + (0.5 * height)) && (y + (0.5 * height)) < (ptr->gety() + (0.5 * ptr->getheight()))) {
+    if ((ptr->gety() - (0.5 * ptr->getheight())) < (y + (0.5 * height)) &&
+                                                   (y + (0.5 * height)) < (ptr->gety() + (0.5 * ptr->getheight()))) {
       overlapTest++;
     }
-    if ((ptr->gety() - (0.5 * ptr->getheight())) < (y - (0.5 * height)) && (y - (0.5 * height)) < (ptr->gety() + (0.5 * ptr->getheight()))) {
+    if ((ptr->gety() - (0.5 * ptr->getheight())) < (y - (0.5 * height)) &&
+                                                   (y - (0.5 * height)) < (ptr->gety() + (0.5 * ptr->getheight()))) {
       overlapTest++;
     }
 
-    
-    if (overlapTest > 0) {
+    //For overlap to happen 2 of these have to be true if 1, 3, or 4 are
+    //output it either isn't a rectangle anymore or a weird bug.
+    if (overlapTest == 2) {
       return true;
     }
     else {
@@ -94,8 +106,8 @@ public:
 
 
 int main() {
-  Rectangle2D Rich(0, 0, 4, 4);
+  Rectangle2D Rich(0, 0, 2, 4);
   Rectangle2D Rich2(1, 1, 4, 4);
-  cout << Rich.contains(Rich2) << endl;
+  cout << Rich.getArea() << endl;
   return 0;
 }
